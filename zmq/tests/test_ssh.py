e55072999d1c73b40422df63f94569b3eda0fafa
Ioannis Tziakos <ioannist@enthought.com>
2017-05-09 02:43:21 +0100

    mark test for skipping on windows

--- zmq/tests/test_ssh.py
+++ zmq/tests/test_ssh.py
@@ -1,5 +1,9 @@
+import sys
+
+from pytest import mark
 from zmq.ssh.tunnel import select_random_ports
 
+@mark.skipif(sys.platform.startswith('win'), reason='Hangs on Windows')
 def test_random_ports():
     for i in range(4096):
         ports = select_random_ports(10)
